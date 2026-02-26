fe81e683ddf81a04003587a2be2523ba5f164381
yagyadeep <yagyadeepahirwar@gmail.com>
2026-02-26 15:00:13 +0530

    day13

---
+++ javascript/jsday10/part2.js
@@ -0,0 +1,20 @@
+console.log("part 2");
+
+// q3 -> check odd from 1 to 50
+
+// for(let i=1;i<= 50;i++){
+//     if(i%2 == 1){
+//         console.log(i);
+//     }
+// }
+
+//q5
+
+let sum=0;
+let n= 5;
+
+for(let i=1;i<= n;i++){
+    sum= sum + i;
+}
+
+console.log("totle sum is: ", sum);
